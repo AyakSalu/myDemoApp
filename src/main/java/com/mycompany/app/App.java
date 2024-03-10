0ba5c20ca6bd39a99f18b0c2e562986dd8620955
AyakSalu <aewyheq@gmail.com>
2024-03-10 18:43:25 +0300

    final

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -117,7 +117,7 @@ public class App
         get("/compute",
             (rq, rs) -> {
               Map map = new HashMap();
-              map.put("result", "not computed yet!");
+              map.put("result", "henüz hesaplanmadı!");
               return new ModelAndView(map, "compute.mustache");
             },
             new MustacheTemplateEngine());
